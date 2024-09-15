b43fd5b0f89420fa839c34aaa5a04647af24e9e7
Erik Russell <erik.z.russell@gmail.com>
2024-09-15 10:34:52 -0700

    load dictionary and pick a random word

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+# load dictionary
+dictionary = []
+File.foreach('google-10000-english-noswears.txt') do |line|
+  dictionary << line.strip if line.chomp.length.between?(5, 12)
+end
+
+# return randowm word as array of letters
+def get_random_word(dictionary)
+  dictionary[rand(dictionary.count)].split('')
+end
