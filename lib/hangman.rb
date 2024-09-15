76abb3a479420a0ecfb966709ea3dbb9305e7cf2
Erik Russell <erik.z.russell@gmail.com>
2024-09-15 11:44:31 -0700

    seems to function, yay!

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,42 +11,45 @@ def get_random_word(dictionary)
   dictionary[rand(dictionary.count)].split('')
 end
 
-def player_turn
+def player_guess
   loop do
     puts 'please enter a letter as a guess'
     input = gets.chomp
-    if input.match?(/^[A-Za-z]{1}$/)
-      guess = input
-      # guesses.push(guess)
-      break
-    else
-      puts 'Invalid input! Enter a single letter.'
-    end
+    return input if input.match?(/^[A-Za-z]{1}$/)
+
+    puts 'Invalid input! Enter a single letter.'
+  end
+end
+
+puts "Let's play a game of hangman!!!\n\n"
+
+game_over = false
+word_to_guess = get_random_word(dictionary)
+correct_guesses = Array.new(word_to_guess.length) { '_' }
+wrong_guesses = []
+guesses = []
+
+puts "The word is #{word_to_guess.length} letters long"
+puts correct_guesses.join
+until game_over
+  guess = player_guess
+  if guesses.include?(guess)
+    puts 'you already guessed that'
+    next
   end
 
+  guesses.push(guess).sort!
+  puts "your guesses so far: #{guesses}"
   if word_to_guess.include?(guess)
 
     word_to_guess.each_with_index do |letter, index|
       correct_guesses[index] = guess if letter == guess
     end
-    puts correct_guesses.join
   else
     wrong_guesses.push(guess)
   end
 
-  true if correct_guesses == word_to_guess
+  puts correct_guesses.join
+  game_over = true if (correct_guesses == word_to_guess) || (wrong_guesses.length == 8)
 end
-
-puts "Let's play a game of hangman!!!\n\n"
-game_over = false
-
-begin
-  word_to_guess = get_random_word(dictionary)
-  correct_guesses = Array.new(word_to_guess.length) { '_' }
-  wrong_guesses = []
-  guesses = []
-  puts "The word is #{word_to_guess.length} letters long"
-  player_turn until game_over
-rescue StandardError
-  puts 'ERROR'
-end
+puts 'Congratulations!!!'
